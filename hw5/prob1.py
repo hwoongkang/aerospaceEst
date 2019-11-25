d9e22ae4ec12832a58541cdc3d47a6ade132d7c1
hyunwoong kang <hwoongkang@snu.ac.kr>
2019-11-25 22:27:17 +0900

    proper iteration

--- hw5/prob1.py
+++ hw5/prob1.py
@@ -1,36 +1,61 @@
 from numpy import matrix as mat
-from numpy.linalg import inv,eig
+import numpy as np
+from numpy.linalg import inv, eig, norm
 from math import pi, atan, sqrt
+from sys import exit
 
-# measurement
-H = mat([4/25, -1/3 + 3/25])
 
-# meas noise
-V = (0.4 * pi / 180) ** 2
+def h(vec):
+    x = vec[0]
+    y = vec[1]
+    return atan(-y/(3-x))+atan((y+4)/(3-x))
 
-# a priori uncertainty
-M = mat([[0.2**2,0],[0,0.3**2]])
 
-# a posteriori uncertainty
-C = inv(M) + H.transpose()@(mat(1/V))@H
+def H(vec):
+    x = vec[0,0]
+    y = vec[1,0]
+    xx3 = (3-x)**2
+    yy = y**2
+    yy4 = (y+4)**2
 
-# innovation? vector?
-b = -(54.8 * pi/180 - atan(4/3)) * 1/V * H
+    temp = [-y/(xx3+yy) + (y+4)/(xx3+yy4), (x-3)/(xx3+yy) + (3-x)/(xx3+yy4)]
 
-print("b: ",b)
+    return mat(temp)
 
-x_new = -inv(C)@b.transpose()
-print("x_new: ", x_new, '\n')
+def newt(x0,M0,V,z):
+    x = x0
+    M = M0
+    
+    while(True):
+        hx = h(x)
+        Hx = H(x)
+        P = inv(inv(M)+(Hx.transpose()@mat(1/V)@Hx))
+        gr = -(Hx.transpose()@mat(1/V)@mat(z-hx))
+        print("gr: ",norm(gr))
+        if norm(gr)<1E-6:
+            break
+        x = x- P@gr
+    return x,P
+z = 54.8 * pi/180
 
-P = inv(C)
-print("P: ", P,'\n')
+
+
+x0 = mat([[0],[0]])
+
+M0 = mat([[0.2**2, 0], [0, 0.3**2]])
+
+V = (0.4 * pi/180)**2
+
+x,P = newt(x0,M0,V,z)
+print("x:\n",x,"\nP:\n",P)
 
 # error ellipse
 vals, vecs = eig(P)
 
-print("eigenvalues: ", vals, "\neigenvectors:\n",vecs,'\n')
+print("eigenvalues: ", vals, "\neigenvectors:\n", vecs, '\n')
 
-print(atan(vecs[0,0]/vecs[1,0]) * 180/pi)
+print("direction: ",vecs[0,0]/vecs[1,0])
+print(atan(vecs[0, 0]/vecs[1, 0]) * 180/pi)
 
 print(sqrt(vals[0]/vals[1]))
 
